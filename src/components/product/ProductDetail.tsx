cf5157e2130cefd0270a098395d5d178e0b204a7
Noureddine-A <anoxybiosis@hotmail.com>
2024-12-05 22:51:09 +0100

    product detail page responsive

--- src/components/product/ProductDetail.tsx
+++ src/components/product/ProductDetail.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
+import Footer from "../Footer.tsx";
+
 const ProductDetail = () => {
   const [images, setImages] = useState<string[]>([]);
   const [mainImage, setMainImage] = useState<number>(0);
@@ -28,50 +30,71 @@ const ProductDetail = () => {
   }, [pathname]);
 
   return (
-    <div className="grid grid-cols-2 h-fit w-full border-t-2 border-gray-300 pt-[2rem]">
-      <div className="grid grid-cols-[1fr,6fr] gap-[0.5rem] w-full h-[65vh]">
-        <div className="flex flex-col gap-[0.5rem] h-4/5 w-full">
-          {images.map((image, index) => {
-            return (
+    <div>
+      <div>
+        <div className="grid grid-cols-2 h-fit w-full border-t-2 border-gray-300 pt-[2rem] max-lg:grid-cols-none max-lg:block">
+          <div className="grid grid-cols-[1fr,6fr] gap-[0.5rem] w-full h-[65vh] max-lg:grid-cols-none max-lg:h-fit max-lg:justify-center max-lg:mb-[2rem]">
+            <div className="flex flex-col gap-[0.5rem] h-4/5 w-full max-lg:row-start-2 max-lg:flex-row">
+              {images.map((image, index) => {
+                return (
+                  <img
+                    onClick={() => {
+                      setMainImage(index);
+                    }}
+                    className="w-full h-[100px] border-2 border-gray-200 mr-auto hover:cursor-pointer max-lg:h-[60px] max-lg:w-[60px]"
+                    src={image}
+                    alt={image}
+                  />
+                );
+              })}
+            </div>
+            <div className="h-4/5 w-full max-lg:row-start-1 max-lg:flex max-lg:justify-center max-lg:h-full">
               <img
-                onClick={() => {
-                  setMainImage(index);
-                }}
-                className="w-full h-[100px] border-2 border-gray-200 mr-auto hover:cursor-pointer"
-                src={image}
-                alt={image}
+                className="w-4/6 h-[40vh] border-2 border-gray-200 max-lg:w-full"
+                src={images[mainImage]}
+                alt={images[mainImage]}
               />
-            );
-          })}
-        </div>
-        <div className="h-4/5 w-full">
-          <img
-            className="w-4/6 h-full border-2 border-gray-200"
-            src={images[mainImage]}
-            alt={images[mainImage]}
-          />
-        </div>
-      </div>
-      <div className="inline-flex flex-col gap-[1rem] w-fit h-full border-b-2 border-gray-200">
-        <h1 className="text-2xl font-bold">{state.name}</h1>
-        <h1 className="text-3xl font-bold">${state.price}</h1>
-        <p className="text-sm text-gray-600 mb-[1rem] w-[40ch]">A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
-        </p>
-        <div className="w-full h-fit">
-          <p className="text-sm font-bold mb-[1rem]">Select Size</p>
-          {state.sizes.map((size) => {
-            return (
-              <button className="w-[50px] h-[50px] bg-gray-300 mr-[0.5rem] rounded-sm hover:bg-slate-950 hover:text-white hover:rounded-sm">
-                {(size === "Medium" && "M") ||
-                  (size === "Large" && "L") ||
-                  (size === "Extra Large" && "XL") ||
-                  (size === "Small" && "S")}
+            </div>
+          </div>
+          <div className="grid grid-rows-[4fr,1fr] w-full h-full">
+            <div className="inline-flex flex-col gap-[1rem] w-fit h-full border-b-2 border-gray-200 max-lg:pb-[1rem] max-lg:mr-[1rem] max-lg:w-full max-lg:text-center">
+              <h1 className="text-2xl font-bold">{state.name}</h1>
+              <h1 className="text-3xl font-bold">${state.price}</h1>
+              <p className="text-sm text-gray-600 mb-[1rem] w-[40ch] max-lg:w-full">
+                A lightweight, usually knitted, pullover shirt, close-fitting
+                and with a round neckline and short sleeves, worn as an
+                undershirt or outer garment.
+              </p>
+              <div className="w-full h-fit">
+                <p className="text-sm font-bold mb-[1rem]">Select Size</p>
+                {state.sizes.map((size) => {
+                  return (
+                    <button className="w-[50px] h-[50px] bg-gray-300 mr-[0.5rem] rounded-sm hover:bg-slate-950 hover:text-white hover:rounded-sm">
+                      {(size === "Medium" && "M") ||
+                        (size === "Large" && "L") ||
+                        (size === "Extra Large" && "XL") ||
+                        (size === "Small" && "S")}
+                    </button>
+                  );
+                })}
+              </div>
+              <button className="bg-slate-950 text-white h-[40px] w-[150px] text-sm mt-[2rem] max-lg:justify-center max-lg:m-auto">
+                ADD TO CART
               </button>
-            );
-          })}
+            </div>
+            <div className="w-full h-fit mt-[1rem] max-lg:text-center max-lg:pb-[2rem]">
+              <p className="text-sm text-gray-400">100% Original product</p>
+              <p className="text-sm text-gray-400">
+                Cash on delivery is available on this product.
+              </p>
+              <p className="text-sm text-gray-400">
+                Easy return and exchange policy within 7 days.
+              </p>
+            </div>
+          </div>
         </div>
-        <button className="bg-slate-950 text-white h-[40px] w-[150px] text-sm mt-[2rem]">ADD TO CART</button>
       </div>
+      <Footer />
     </div>
   );
 };
